15c764e0724ffa7a3a7debea7a7d9751a8cc0064
xinluomed_yuxuecheng <xinluomed_yuxuecheng@git.cloud.tencent.com>
2018-11-30 19:56:02 +0800

    Modify the function.

--- include/lac_main.h
+++ include/lac_main.h
@@ -16,7 +16,7 @@ extern "C" {
 /// \param content, the content need to be tagged
 /// \return tagged results, or ""
 ///
-const char* cut_sentence(const char* conf_dir, int max_result_num, const char* content);
+char* cut_sentence(const char* conf_dir, int max_result_num, const char* content);
 
 int sum(int a, int b);
 
